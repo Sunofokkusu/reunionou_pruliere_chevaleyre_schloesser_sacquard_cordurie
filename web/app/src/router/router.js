77be38b622d3acc416ade3521912726b0427b9f4
Lhiikan <antoine.chevaleyre54@gmail.com>
2023-03-27 16:06:44 +0200

    page de connexion et d'inscription

--- web/app/src/router/router.js
+++ web/app/src/router/router.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router';
 import NotFound from "@/components/NotFound";
 import HomePage from "@/components/HomePage";
 import SignIn from "@/components/SignIn";
+import SignUp from "@/components/SignUp";
 
 const routes = [
     { name: 'NotFound', path: '/:pathMatch(.*)*', component: NotFound },
     { name: 'HomePage', path: '/', component: HomePage },
     { name: 'signIn', path: '/signin', component: SignIn },
+    { name: 'signUp', path: '/signup', component: SignUp },
 ];
 
 const router = createRouter({
