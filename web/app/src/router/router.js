f564a7ba26ca30329bc726345c08627895f2eb04
Lhiikan <antoine.chevaleyre54@gmail.com>
2023-03-28 16:14:48 +0200

    creation d'event

--- web/app/src/router/router.js
+++ web/app/src/router/router.js
@@ -3,10 +3,12 @@ import NotFound from "@/components/NotFound";
 import HomePage from "@/components/HomePage";
 import SignIn from "@/components/SignIn";
 import SignUp from "@/components/SignUp";
+import EventPage from "@/components/EventPage";
 
 const routes = [
     { name: 'NotFound', path: '/:pathMatch(.*)*', component: NotFound },
     { name: 'HomePage', path: '/', component: HomePage },
+    { name: 'Event', path: '/event/:event_token', component: EventPage },
     { name: 'signIn', path: '/signin', component: SignIn },
     { name: 'signUp', path: '/signup', component: SignUp },
 ];
