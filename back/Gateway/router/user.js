434a82a419fad45f9c048d1bd93086fd4515e2e7
CordurieL <82098372+CordurieL@users.noreply.github.com>
2023-03-29 18:09:32 +0200

    fix

--- back/Gateway/router/user.js
+++ back/Gateway/router/user.js
@@ -2,44 +2,49 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 
-router.get('/me', async (req, res, next) => {
-    try{
-        if(!req.headers.authorization) return next({error : 401, message : "Unauthorized"});
-        let validate = await axios.post(process.env.USER_SERVICE + "validate", {}, {
-            headers: {
-                Authorization: req.headers.authorization
-            }
-        });
-        let events = await axios.get(process.env.EVENT_SERVICE + "user/" + validate.data.id);
-        let invited = await axios.get(process.env.EVENT_SERVICE + "user/" + validate.data.id + "/invited");
-        if(req.query.embed === "events"){
-            res.json({
-                name : validate.data.name,
-                mail : validate.data.email,
-                events : events.data
-            });
-        }else if(req.query.embed === "invited"){
-            res.json({
-                name : validate.data.name,
-                mail : validate.data.email,
-                invited : invited.data
-            });
-        }else if(req.query.embed === "all"){
-            res.json({
-                name : validate.data.name,
-                mail : validate.data.email,
-                events : events.data,
-                invited : invited.data
-            });
-        }else{
-            res.json({
-                name : validate.data.name,
-                mail : validate.data.email,
-            });
-        }
-    }catch(err){
-        console.log(err);
-        return next(err.response.data);
+router.get("/me", async (req, res, next) => {
+  try {
+    if (!req.headers.authorization)
+      return next({ error: 401, message: "Unauthorized" });
+    let validate = await axios.post(
+      process.env.USER_SERVICE + "validate",
+      {},
+      {
+        headers: {
+          Authorization: req.headers.authorization,
+        },
+      }
+    );
+    let events = await axios.get(
+      process.env.EVENT_SERVICE + "user/" + validate.data.id
+    );
+    let invited = await axios.get(
+      process.env.EVENT_SERVICE + "user/" + validate.data.id + "/invited"
+    );
+    if (req.query.embed === "events") {
+      res.json({
+        name: validate.data.name,
+        mail: validate.data.email,
+        events: events.data,
+      });
+    } else if (req.query.embed === "invited") {
+      res.json({
+        name: validate.data.name,
+        mail: validate.data.email,
+        invited: invited.data,
+      });
+    } else if (req.query.embed === "all") {
+      res.json({
+        name: validate.data.name,
+        mail: validate.data.email,
+        events: events.data,
+        invited: invited.data,
+      });
+    } else {
+      res.json({
+        name: validate.data.name,
+        mail: validate.data.email,
+      });
     }
   } catch (err) {
     console.log(err);
--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -4,10 +4,14 @@ import 'package:reunionou/auth_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter/services.dart';
 
 void main() async {
   // await dotenv.load(fileName: "assets/.env");
   WidgetsFlutterBinding.ensureInitialized();
+  // hide the bottom bar on android setEnabledSystemUIMode
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+      overlays: [SystemUiOverlay.bottom]);
 
   // await Supabase.initialize(
   //   url: dotenv.env['SUPABASE_URL']!,
